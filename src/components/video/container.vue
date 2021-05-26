806c9bb3e3388bbc8ee09b3795eb6c90f359f6c9
LoTwT <709937065@qq.com>
2021-05-27 00:46:33 +0800

    video container

--- src/components/video/container.vue
+++ src/components/video/container.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="video-container">
     <video
+      ref="videoTag"
       :src="props.src"
       style="top: 0"
+      autoplay
       loop
       playsinline
       muted
@@ -11,12 +13,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 
 export default defineComponent({
   props: ["src"],
-  setup(props, context) {
-    return { props };
+  setup(props) {
+    const videoTag = ref<any | null>(null);
+
+    onMounted(() => {
+      const videoElement = videoTag.value;
+      videoElement &&
+        videoElement.addEventListener("canplay", () => {
+          // 视频高度
+          const videoHeight: number = videoElement.offsetHeight;
+          // 屏幕高度
+          const screenHeight: number = document.documentElement.clientHeight;
+          // 使视频上下居中
+          videoElement.style.top = (screenHeight - videoHeight) / 2 + "px";
+          // 防止 autoplay 在网速过慢时失效
+          videoElement.play();
+        });
+    });
+
+    return { props, videoTag };
   },
 });
 </script>
